17cdf89315890c115848efdd7f8ab2c43aef9dfa
Dreaming-world <2498219840@qq.com>
2019-11-03 03:47:28 +0800

    update

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from flask import Flask
 from flask import render_template
-from flask import url_for
+from src.read_qa import read_qa
+from flask import request
 
+qa_path = 'qa_source/qa.txt'
 app = Flask(__name__)
+
+user_name = None
+user_id = None
 
 
 @app.route('/')
@@ -10,9 +15,27 @@ def hello_world():
     return render_template('login.html')
 
 
-@app.route('/question')
+@app.route('/question', methods = ['POST', 'GET'])
 def question():
-    return render_template('question.html')
+    # TODO
+    global user_name
+    global user_id
+    if request.method == 'POST':
+        user_name = request.form['user_name']
+        user_id = request.form['user_id']
+    print(user_name)
+    singal_dict, double_dict, fill_dict = read_qa(qa_path, choice = [2, 1, 1])
+    # print(singal_dict)
+    # print(double_dict)
+    # print(fill_dict)
+    qa = {
+        'user_name': user_name,
+        'user_id': user_id
+    }
+    return render_template('question.html', qa=qa,
+                           singal_dict=singal_dict,
+                           double_dict=double_dict,
+                           fill_dict=fill_dict)
 
 
 if __name__ == '__main__':
--- src/read_qa.py
+++ src/read_qa.py
@@ -1,10 +1,26 @@
 from collections import defaultdict
+import numpy as np
 
 '''
 dict key--index [0 1] [0]--qa [1] question
 '''
+chart_list = ['B', 'C', 'D', 'E', 'F']
 
-def read_qa(file_path):
+
+def split_answer(answer):
+    result = []
+    index = 0
+    for key in chart_list:
+        if key in answer:
+            ele = answer.split(key)[0]
+            answer = answer[len(ele):]
+            result.append(ele)
+            index = index + 1
+    result.append(answer)
+    return result
+
+
+def read_qa(file_path, choice=[]):
     singal_dict = defaultdict(list)
     double_dict = defaultdict(list)
     fill_dict = defaultdict(list)
@@ -14,7 +30,7 @@ def read_qa(file_path):
     flag_singal = 0
     flag_double = 0
     flag_fill = 0
-    with open('../qa_source/qa.txt', "r") as f:
+    with open(file_path, "r") as f:
         line = f.readline().replace('\n', '').replace('\t', '')
         while line:
             if "单选题" in line:
@@ -36,40 +52,61 @@ def read_qa(file_path):
                 # line = f.readline()
                 if 'A' not in line and 'B' not in line and 'C' not in line and 'D' not in line:
                     index_singal += 1
-                    qa = line
-                    print(qa)
+                    qa = line.split('.')[1]
+                    # print(qa)
                     singal_dict[index_singal].append(qa)
                     line = f.readline().replace('\n', '').replace('\t', '')
                     answer = ''
                     while 'A' in line or 'B' in line or 'C' in line or 'D' in line:
-                        answer += line + ' '
+                        answer += line
                         line = f.readline().replace('\n', '').replace('\t', '')
-                    singal_dict[index_singal].append(answer.split())
-                    print(answer)
+                    singal_dict[index_singal].append(split_answer(answer.replace(' ', '')))
+                    # print(answer)
 
             if flag_double:
                 # line = f.readline()
                 if 'A' not in line and 'B' not in line and 'C' not in line and 'D' not in line:
                     index_double += 1
-                    qa = line
-                    print(qa)
+                    qa = line.split('.')[1]
+                    # print(qa)
                     double_dict[index_double].append(qa)
                     line = f.readline().replace('\n', '').replace('\t', '')
                     answer = ''
                     while 'A' in line or 'B' in line or 'C' in line or 'D' in line or 'E' in line:
-                        answer += line +' '
+                        answer += line + ' '
                         line = f.readline().replace('\n', '').replace('\t', '')
-                    double_dict[index_double].append(answer)
-                    print(answer)
+                    double_dict[index_double].append(split_answer(answer.replace(' ', '')))
+                    # print(answer)
 
             if flag_fill:
                 index_fill += 1
-                fill_dict[index_fill].append(line)
-                print(line)
+                fill_dict[index_fill].append(''.join(line.split('.')[1:]))
+                # print(line)
                 line = f.readline().replace('\n', '').replace('\t', '')
-    return singal_dict, double_dict, fill_dict
+    # TODO
+    single_choice = np.random.choice(a=index_singal, size=choice[0], replace=False, p=None)
+    double_choice = np.random.choice(a=index_double, size=choice[1], replace=False, p=None)
+    fill_choice = np.random.choice(a=index_fill, size=choice[2], replace=False, p=None)
+    dict_singal = defaultdict()
+    dict_double = defaultdict()
+    dict_fill = defaultdict()
+    new_index = 1
+    for index in single_choice:
+        dict_singal[new_index] = singal_dict[index + 1]
+        new_index += 1
+    new_index = 1
+    for index in double_choice:
+        dict_double[new_index] = double_dict[index + 1]
+        new_index += 1
+    new_index = 1
+    for index in fill_choice:
+        dict_fill[new_index] = fill_dict[index + 1]
+        new_index += 1
+    return dict_singal, dict_double, dict_fill
 
 
 if __name__ == "__main__":
-    singal_dict, double_dict, fill_dict = read_qa('hh')
+    file_path = '../qa_source/qa.txt'
+    dict_singal, dict_double, dict_fill = read_qa(file_path, choice=[2, 1, 1])
+    print(dict_singal, dict_double, dict_fill)
 
